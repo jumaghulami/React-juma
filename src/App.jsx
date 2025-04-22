2bf3392bd80ca6d2611fc58e7d3735eee9b95e12
Juma Ghulami <jumaghulami313@gmail.com>
2025-04-22 08:58:37 +0430

    add some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import About from "./aboutUs/About";
@@ -6,6 +6,9 @@ import Contact from "./contactUs/Contact";
 import Navbar from "./shop-bike/navbar/Navbar";
 import Footer from "./shop-bike/footer/Footer";
 import Services from "./shop-bike/services/Services";
+import Project1 from "./project/project1/Project1";
+import Project2 from "./project/project2/Project2";
+import Project3 from "./project/project3/Project3";
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/project1" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
+          <Route path="/project3" element={<Project3 />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/project/project2/Project2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project2() {
+  return (
+    <div>
+      <div>welcome project 2</div>
+    </div>
+  );
+}
+
+export default Project2;
